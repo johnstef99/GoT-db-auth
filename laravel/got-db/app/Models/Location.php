cae9b084f8f9d2667432322241063b04f46fc154
johnstef99 <john@johnstef.com>
2021-12-16 00:53:28 +0200

    add events to locations

--- laravel/got-db/app/Models/Location.php
+++ laravel/got-db/app/Models/Location.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Location extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    public function houses(): HasMany
-    {
-        return $this->hasMany(House::class);
-    }
+  public function houses(): HasMany
+  {
+    return $this->hasMany(House::class);
+  }
+
+  public function events(): BelongsToMany
+  {
+    return $this->belongsToMany(NotableEvent::class, 'location_notable_event');
+  }
 }
