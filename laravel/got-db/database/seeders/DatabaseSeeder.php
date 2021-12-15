5e9c096ee609435768baa469316caad688c8e29b
johnstef99 <john@johnstef.com>
2021-12-16 00:53:56 +0200

    create a basic seeder

--- laravel/got-db/database/seeders/DatabaseSeeder.php
+++ laravel/got-db/database/seeders/DatabaseSeeder.php
@@ -2,20 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Character;
 use App\Models\House;
+use App\Models\Religion;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $house = new House();
-        $name = $house->name;
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+
+    $firstChar = Character::factory()->create();
+
+    $houseCreated = $firstChar->house;
+
+
+    $religionCreated = $firstChar->religion;
+
+    Character::factory()->count(10)->for($houseCreated)->for($religionCreated)->create();
+  }
 }
